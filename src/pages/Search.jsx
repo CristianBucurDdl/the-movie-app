15e12aa39642c6627b4bdd77e4a71bf9443fe3f6
Cristian Bucur <cristian.bucur@dedalus.eu>
2023-10-31 16:56:22 +0000

    added search api

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,15 +1,35 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { getRequestedMovies } from "../services/search_movie";
+import axios from "axios";
 
 export const SearchPage = () => {
   const [searchBy, setSearchBy] = useState("");
-
+  const [returnedSearchMovies, setReturnedSearchMovies] = useState([
+    "notModified",
+  ]);
   const defineSearchCriteria = (e) => {
     setSearchBy(e.target.value);
   };
 
-  const search = () => {
-    console.log(searchBy);
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1MTdmOWY1YjRiNDc1MzJhNWQ1NzNjZmJhYTNjNTU2YyIsInN1YiI6IjYzMWY0ZGM5ZjYyMWIyMDA3YzA3OGYzYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.sAWGFzpnkcnw912_wJD-glAjUWOTqiMNvA4zT2fFmW4",
+    },
   };
+  const search = () => {
+    axios
+      .get(
+        `https://api.themoviedb.org/3/search/movie?query=${searchBy}&include_adult=true&language=en-US`,
+        options
+      )
+      .then((response) => {
+        setReturnedSearchMovies(response.data.results);
+      });
+  };
+  console.log(returnedSearchMovies);
   return (
     <div>
       Search
