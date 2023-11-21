a278237aba92bff3a50bf17753767e9a4fca2d81
Cristian Bucur <cristian.bucur@dedalus.eu>
2023-11-21 15:43:24 +0000

    refractored navigation component

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,96 +1,35 @@
 import { Link } from "react-router-dom";
-import styled from "styled-components";
-// import { ReactComponent as Logo } from ;
-import BurgerIcon from "../assets/burger.svg?react";
+
 import SearchSvg from "../assets/search.svg?react";
-const SearchIcon = () => {
-  return <SearchSvg />;
-};
-const NavigationContainer = styled.div`
-  background-color: #121212;
-  padding: 10px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-
-  @media (max-width: 768px) {
-    flex-direction: column;
-    position: fixed;
-    bottom: 0;
-    width: 100%;
-    z-index: 999;
-  }
-`;
-
-const BurgerButton = styled.button`
-  background: none;
-  border: none;
-  cursor: pointer;
-  display: none; // Hide by default on larger screens
-
-  @media (max-width: 768px) {
-    display: block; // Show on smaller screens
-  }
-`;
-
-const MenuButtons = styled.div`
-  display: flex;
-
-  @media (max-width: 768px) {
-    flex-direction: row;
-    justify-content: space-between;
-    align-items: center;
-    width: 80%;
-  }
-`;
-
-const Button = styled.button`
-
-  background-color: transparent;
-  color: white;
-  border: none;
-  padding: 10px;
-  margin: 5px;
-  cursor: pointer;
-  wi &:hover {
-    background-color: #456580;
-  }
-  svg {
-    width: 1rem; /* Set width to your desired dimension */
-    height: 1rem; /* Set height to your desired dimension */
-    fill: white; /* Set fill color to your desired color */
-    margin-right: 1rem; /* Adjust spacing between the icon and text */
-  }
-  @media (max-width: 768px) {
-    /* Hide text on small screens */
-    span {
-      display: none;
-    }
-`;
+import HomeSvg from "../assets/home.svg?react";
+import StarSvg from "../assets/star.svg?react";
+import {
+  Button,
+  MenuButtons,
+  NavigationContainer,
+} from "./styleComponents/styledComponents";
 
 export default function Header() {
-  const handleBurgerClick = () => {
-    // Add logic for handling burger icon click
-    console.log("Burger clicked!");
-  };
   return (
     <NavigationContainer>
-      {/* <BurgerButton onClick={handleBurgerClick}>
-    
-        <BurgerIcon />
-      </BurgerButton> */}
       <MenuButtons>
         <Link to={"/"}>
-          <Button>Home</Button>
+          <Button>
+            <HomeSvg />
+            <span>Home</span>
+          </Button>
         </Link>
         <Link to={"/search"}>
           <Button>
-            <SearchIcon />
+            <SearchSvg />
             <span>Search</span>
           </Button>
         </Link>
         <Link to={"/favorite"}>
-          <Button>Favorite</Button>
+          <Button>
+            <StarSvg />
+            <span>Favorite</span>
+          </Button>
         </Link>
       </MenuButtons>
     </NavigationContainer>
--- src/components/styleComponents/styledComponents.js
+++ src/components/styleComponents/styledComponents.js
@@ -19,3 +19,53 @@ export const MovieItem = styled.div`
   width: 11rem;
   height: 10rem;
 `;
+export const NavigationContainer = styled.div`
+  background-color: #121212;
+  padding: 10px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+    z-index: 999;
+  }
+`;
+
+export const MenuButtons = styled.div`
+  display: flex;
+
+  @media (max-width: 768px) {
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    width: 80%;
+  }
+`;
+
+export const Button = styled.button`
+
+  background-color: transparent;
+  color: white;
+  border: none;
+  padding: 10px;
+  margin: 5px;
+  cursor: pointer;
+  wi &:hover {
+    background-color: #456580;
+  }
+  svg {
+    width: 1rem; /* Set width to your desired dimension */
+    height: 1rem; /* Set height to your desired dimension */
+    fill: white !important; /* Set fill color to your desired color */
+    margin-right: 1rem; /* Adjust spacing between the icon and text */
+  }
+  @media (max-width: 768px) {
+    /* Hide text on small screens */
+    span {
+      display: none;
+    }
+`;
